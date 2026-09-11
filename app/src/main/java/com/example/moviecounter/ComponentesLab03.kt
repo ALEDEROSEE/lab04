0eef82775b72cc8c75791c2f1efd94f49ebaeed2
Alejandro Aaron Alvarez Sayaverde <sayaverde2000@gmail.com>
2026-09-11 16:19:06 -0500

    feat: reutiliza componente Switch del catalogo del lab03

--- app/src/main/java/com/example/moviecounter/ComponentesLab03.kt
+++ app/src/main/java/com/example/moviecounter/ComponentesLab03.kt
@@ -5,6 +5,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
 import androidx.compose.material3.Checkbox
+import androidx.compose.material3.Switch
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,4 +35,10 @@ fun ComponenteCheckboxLab03() {
         Checkbox(checked = checked, onCheckedChange = { checked = it })
         Text("Opcion seleccionable")
     }
+}
+
+@Composable
+fun ComponenteSwitchLab03() {
+    var on by remember { mutableStateOf(true) }
+    Switch(checked = on, onCheckedChange = { on = it })
 }
